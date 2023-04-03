27256cde3b3bdfdd504bdc7797ea79806c7d2a72
shr-skopos <Ihre E-Mail-Adresse>
2023-04-03 13:55:51 +0200

    get .env value

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './config/config.env' });
 
 // express() = Aufruf der Hauptfunktion des Frameworks
 const APP = express();
 const IP = "127.0.0.1";
-const PORT = 8000;
-console.log("Hi my Teams")
+const PORT = process.env.SERVER_PORT || 8000;
+const smtpPort = process.env.SMTP_PORT || 443 || 465 || 25;
+
+console.log(smtpPort)
 
 // Server starten
 APP.listen(PORT, IP, () => console.log(`http://${IP}:${PORT}`))
