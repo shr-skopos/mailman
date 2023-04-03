7d1125855fbd7d3e0730d0a835c0b9122e16b78b
shr-skopos <Ihre E-Mail-Adresse>
2023-04-03 13:59:57 +0200

    add mail sender

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 import { promises as fs } from 'fs';
 
@@ -7,7 +8,7 @@ dotenv.config({ path: './config/config.env' });
 // express() = Aufruf der Hauptfunktion des Frameworks
 const APP = express();
 const IP = "127.0.0.1";
-const PORT = process.env.SERVER_PORT || 8000;
+const PORT = process.env.SERVER_PORT || 3000;
 const smtpPort = process.env.SMTP_PORT || 443 || 465 || 25;
 
 // Verzeichnis für statische Dateien
@@ -18,12 +19,38 @@ APP.post('/send-email', async (req, res) => {
     try {
         // Lade den Inhalt der email_template.html-Datei
         const emailContent = await fs.readFile('publich/email_template.html', 'utf8');
-        res.send(emailContent);
+        
+        // Konfiguration für den Mail-Transport
+        const transporter = nodemailer.createTransport({
+            service: process.env.SMTP_HOST || 'gmail',
+            port:smtpPort,
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASS,
+            },
+        });
+
+        // Konfiguriere die E-Mail-Optionen
+        const mailOptions = {
+            from:  process.env.MAIL_FROM,
+            to: process.env.MAIL_TO,
+            subject: 'Ein Beispielprojekt, das Mailmann von Saied Hr :)',
+            html: emailContent,
+        };
+
+        // // Versende die E-Mail
+        await transporter.sendMail(mailOptions);
+
+        // Senden Sie eine Bestätigungsantwort an den Client
+        res.send('E-Mail gesendet!');
 
     } catch (error) {
         console.error('Fehler beim Senden der E-Mail:', error);
+        res.status(500).send('Fehler beim Senden der E-Mail!');
     }
 });
 
 // Server starten
 APP.listen(PORT, IP, () => console.log(`http://${IP}:${PORT}`))
+
+
