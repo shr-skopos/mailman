4d694eb96daf0344641897782e87a8dbd5411985
shr-skopos <Ihre E-Mail-Adresse>
2023-04-03 13:39:24 +0200

    add structure

---
+++ demo.d
@@ -0,0 +1,15 @@
+mailman/
+├── config/
+│   └── config.env
+├── publich/
+│   ├── email_template.html
+│   ├── email_template.js
+│   ├── style.css
+│   └── index.html
+├── node_modules/
+│   ├── dotenv-extended/
+│   ├── nodemailer/
+│   └── express/
+├── server.js
+├── package-lock.json
+└── package.json
